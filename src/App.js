24c3e9abde559daeffe76f85ae48dfc695dca4b9
bkfarnsworth <bkfarnsworth@gmail.com>
2022-08-16 18:31:48 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import storyTree from "./storyTree";
+import "./styles.css";
+
+export default function App() {
+  // useState is a helper function to keep track of what branch the user is at
+  let [branch, setBranch] = useState(storyTree.root);
+  // we can also keep track of the text they type in
+  let [text, setText] = useState("");
+
+  return (
+    <div className="App">
+      <h1>Welcome to Space Adventure!</h1>
+      <div>{branch.prompt}</div>
+      <br />
+      <div>
+        <input value={text} onChange={(e) => setText(e.target.value)} />
+      </div>
+      <br />
+      <button onClick={() => handleSubmit(text, branch, setBranch, setText)}>
+        Submit
+      </button>
+    </div>
+  );
+}
+
+// when the user hits the submit button
+// try to find a branch that matches the text they typed in
+// if found, set the current branch to the matching branch
+// else do something else...
+function handleSubmit(submittedText, currentBranch, setBranch, setText) {
+  let chosenBranch = currentBranch.branches.find((b) =>
+    submittedText.includes(b.trigger)
+  );
+  if (chosenBranch) {
+    setBranch(chosenBranch);
+  } else {
+    alert("no branch found, what should we show the user next?");
+  }
+
+  // clear out text for next round
+  setText("");
+}
---
+++ src/storyTree.js
@@ -0,0 +1,28 @@
+const storyTree = {
+  root: {
+    prompt: "You have crash landed on a strange planet.  What do you do?",
+    type: "free-response",
+    branches: [
+      {
+        trigger: "badge",
+        prompt:
+          "You open your comm badge and find a dilithium crystal. What do you do with the dilithium crystal?",
+        type: "multiple-choice",
+        branches: [
+          { trigger: "use the crystal as fuel" },
+          { trigger: "use the crystal as food" },
+          { trigger: "use the crystal as a light source" },
+          { trigger: "use the crystal as shovel to dig for minerals" }
+        ]
+      },
+      {
+        trigger: "phaser",
+        prompt:
+          "You open your phaser and find an omega particle.  What do you do with the particle?",
+        type: "free-response"
+      }
+    ]
+  }
+};
+
+export default storyTree;
